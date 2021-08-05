fd61946dd37a089fcb7dad690fa646350ffdcf47
Alonso Quijano <aquijanoruiz@gmail.com>
2021-08-05 17:11:13 -0500

    wrangle update

--- R_scripts/wrangle_disability.R
+++ R_scripts/wrangle_disability.R
@@ -1,30 +1,52 @@
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 
+# -------------------- disability variables --------------------
+
+# Q: Do you have difficulty seeing (even with glasses)? hearing (even with a hearing aid)? 
+# walking or climbing stairs? remembering or concentrating? bathing or dressing? 
+# communicating, understanding or being understood? 1 (not difficult at all) 4 (extremely difficult)
+
 people <- mutate(people, vision = f1_s2_10_1, hearing = f1_s2_10_2, walking_stairs = f1_s2_10_3, 
          cognitive = f1_s2_10_4, bathing_dressing = f1_s2_10_5, communication = f1_s2_10_6)
-
 index <- c("vision", "hearing", "walking_stairs", "cognitive", "bathing_dressing", "communication")
 people[,index] <- sapply(people[,index], FUN = as.integer)
+
+# disabled people -> true for anyone who answered 2 or above in the disability question
 people$disabled <- !apply(people[,index], 1, function(x) max(x)) == 1
 
+# disability degree -> the maximum value among all kinds of disability
 people$dis_degree <- apply(people[,index], 1, function(x) max(x))
 people$dis_degree <- factor(people$dis_degree, levels = c("1", "2", "3", "4"))
 
-set.seed(2525)
+# disability type -> the name of the disability with the maximum value among all kinds of disability
+set.seed(2525) # if there are two or more types of disability with the same value, one is chosen at random
 people$dis_type <- ifelse(!disabled, NA, colnames(people[,index])[max.col(people[,index], ties.method="random")])
 people$dis_type <- factor(people$dis_type, levels = index)
 
 attach(people)
+
+# disability id -> true if the person with disability has a disability id issued by the government
 people$dis_id <- case_when(is.na(disabled) | !disabled ~ NA, f1_s2_11 == "si" ~ TRUE, TRUE ~ FALSE)
+
+# disability percentage -> the disability percentage shown on the disability id
 people$dis_id_percent <- f1_s2_12
 
+# Mision Manuela Espejo -> true if the person with disability was visited by Mision Manuela Espejo
 people$dis_manuela <- case_when(is.na(disabled) | !disabled ~ NA, f1_s2_13 == "si" ~ TRUE, TRUE ~ FALSE)
 
+# disability transfer -> true if anyone in the household receives disability transfers
+# bdh transfer -> true if anyone in the household receives cash transfers (bono de desarrollo humano)
 people <- left_join(people, people %>% group_by(id_hogar) %>% 
                       summarize(dis_transfer = any(f1_s3_29 == "si", na.rm = TRUE),
                                 bdh_transfer = any(f1_s3_27 == "si", na.rm = TRUE)), by = "id_hogar")
 
-people <- mutate(people, age = edadanios, ethnicity = f1_s2_9, marital_status = f1_s2_16, education = f1_s2_19_1)
+# -------------------- sociodemographic variables --------------------
+
+# sex, age, area, ethnic identity, narital status, education
+people <- mutate(people, sex = sexo, age = edadanios, ethnicity = f1_s2_9, marital_status = f1_s2_16, education = f1_s2_19_1)
+
+levels(people$sex) <- c("male", "female")
+levels(people$area) <- c("urban", "rural")
 
 levels(people$ethnicity) <-  c("indigenous", "black", "black", "mestizo", "mestizo", "mestizo", "white", "mestizo")
 people$ethnicity <- factor(people$ethnicity, levels = c("mestizo", "white", "black", "indigenous"))
@@ -37,35 +59,54 @@ levels(people$education) <- c("none", "none", "none", "primary", "primary", "sec
                               "tertiary", "tertiary", "tertiary")
 people$education <- factor(people$education, levels = c("none", "primary", "secondary", "tertiary"))
 
-levels(people$area) <- c("urban", "rural")
-
-index <- c("f1_s3_15", "f1_s3_16_2", "f1_s3_17", "f1_s3_18", "f1_s3_19", "f1_s3_20_2", "f1_s3_21", "f1_s3_22_2")
-people[, index] <- sapply(people[, index], function(x){ 
-  x = ifelse(x == 999999 | is.na(x), 0, x)
-  return(x)
-})
-  
-people <- mutate(people, inc_business_owner = f1_s3_15 + f1_s3_16_2 - f1_s3_17,
-                 inc_employed = f1_s3_18 + f1_s3_19 + f1_s3_20_2,
-                 inc_secondary = f1_s3_21 + f1_s3_22_2,
-                 inc_total = inc_business_owner + inc_employed + inc_secondary)
-
-people$nonemployed <- case_when(is.na(f1_s3_1) ~ NA, as.integer(f1_s3_1) == 1 ~ FALSE, as.integer(f1_s3_2) != 12 ~ FALSE, 
-                                as.integer(f1_s3_3) == 1 ~ FALSE, TRUE ~ TRUE)
-
-personas$trabaja <- factor(personas$trabaja, levels = c("no", "si"))
-
-people$sick <- f1_s4_2 == "si"
-people$prev_med <- f1_s4_41 == "si"
-people$hospital <- f1_s4_54 == "si"
-
-people$good_health <- between(as.integer(f1_s4_58),1,3) 
-people$better_health <- as.integer(f1_s4_59) == 1
-
+# number of children -> number of children a person has
 people <- people %>% mutate(person = as.integer(persona), mother = f1_s2_15_1, father = f1_s2_14_1) %>% 
   group_by(id_hogar) %>% mutate(n_child_mom = sapply(person, function(x) sum(mother %in% x, na.rm = TRUE)),
                                 n_child_dad = sapply(person, function(x) sum(father %in% x, na.rm = TRUE)),
                                 n_child = n_child_mom + n_child_dad)
+
+# -------------------- income variables --------------------
+
+# income is calculated as someones income as a business owner and employed worker in the previous month
+index <- c("f1_s3_15", "f1_s3_16_2", "f1_s3_17", "f1_s3_18", "f1_s3_19", "f1_s3_20_2", "f1_s3_21", "f1_s3_22_2")
+people[, index] <- sapply(people[, index], function(x){ 
+  x = ifelse(x == 999999 | is.na(x), 0, x) # changes NAs and 999999s into zero
+  return(x)
+})
+  
+people <- mutate(people, inc_business_owner = f1_s3_15 + f1_s3_16_2 - f1_s3_17, # income minus expenses
+                 inc_employed = f1_s3_18 + f1_s3_19 + f1_s3_20_2, # salary + benefits
+                 inc_secondary = f1_s3_21 + f1_s3_22_2, # secondary income
+                 inc_total = inc_business_owner + inc_employed + inc_secondary)
+
+# -------------------- outcome variables --------------------
+
+# unemployed -> true if the person did not do any job the previous week and does not have any job to return to
+people$nonemployed <- case_when(is.na(f1_s3_1) ~ NA, as.integer(f1_s3_1) == 1 ~ FALSE, as.integer(f1_s3_2) != 12 ~ FALSE, 
+                                as.integer(f1_s3_3) == 1 ~ FALSE, TRUE ~ TRUE)
+
+# sick -> true if the person suffered from any sickness in the last 30 days
+people$sick <- f1_s4_2 == "si"
+
+# preventive medicine -> true if the person received any preventive care in the last 30 days
+people$prev_care <- f1_s4_41 == "si"
+
+# hospitalized -> true if the person was hospitalized in the last 30 days
+people$hospital <- f1_s4_54 == "si"
+
+# good health -> true if the person considers themselves in good, very good, or excellent health
+people$good_health <- between(as.integer(f1_s4_58),1,3)
+
+# good health -> true if the person considers themselves in better health than in the previous year
+people$better_health <- as.integer(f1_s4_59) == 1
+
+# -------------------- filtering the data --------------------
+
+data <- select(people, id_hogar, id_per, fexp, sex, age, prov, area, ethnicity, education, marital_status, 
+               n_child, inc_business_owner, inc_employed, inc_secondary, inc_total, bdh_transfer, nonemployed,
+               vision, hearing, walking_stairs, cognitive, bathing_dressing, communication, disabled, dis_degree, 
+               dis_type, dis_id, dis_id_percent, dis_manuela, dis_transfer, sick, prev_care, hospital, good_health, 
+               better_health) %>% rename(weight = fexp)
 
 saveRDS(people, file = "rds_files/disability_ec.rds")
 
